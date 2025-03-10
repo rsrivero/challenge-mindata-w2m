4b272470aa4cf3e360a24f9b1bb7d5de44bc5d1b
rodri <rodrigorivero18@gmail.com>
2025-03-10 16:56:33 -0300

    Fixes

--- init-localstack.sh
+++ init-localstack.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 export AWS_REGION=us-east-1
 
-awslocal sqs create-queue --queue-name sae1-spaceship-dev
+QUEUE_NAME="sae1-spaceship-dev"
+ENDPOINT_URL="http://localstack:4566"
+
+if awslocal --endpoint-url="$ENDPOINT_URL" sqs list-queues | grep -q "$QUEUE_NAME"; then
+    echo "La cola $QUEUE_NAME ya existe."
+else
+    awslocal --endpoint-url="$ENDPOINT_URL" sqs create-queue --queue-name "$QUEUE_NAME"
+    echo "Cola $QUEUE_NAME creada con éxito."
+fi
